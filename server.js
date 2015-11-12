8f6d22e46981ab10e417715c61e39a692ca5cf12
James Kusachi <jamesku@zillow.com>
2015-11-11 21:21:02 -0800

    Server update to use koa-handlebars

--- server.js
+++ server.js
@@ -2,40 +2,51 @@
 
 var koa         = require('koa');
 var app         = koa();
-var views       = require('koa-views');
 var router      = require('koa-router')();
 var serve       = require('koa-static');
 var mount       = require('koa-mount');
 var livereload  = require('koa-livereload');
 var koaBody     = require('koa-body');
+var handlebars  = require("koa-handlebars");
 
 
 app.use(livereload());
 
-/* Views */
-app.use(views('public/views', {
-  map: { html: "handlebars" }
-}));
+/* Body */
+app.use(koaBody());
 
+
+/* Static Resources */
+app.use(serve(__dirname + '/app/public'));
+
+/* Handlebars Layouts */
+app.use(handlebars({
+  root: './app',
+  layoutsDir: 'views/layouts',
+  defaultLayout: 'main',
+}))
+
+/* Routes */
 
 app.use( router.routes() );
 
-app.use(koaBody());
-
-//mount
-app.use( mount('/static', serve(__dirname + '/dist')))
- 
-
-// Statics
-app.use(serve(__dirname + '/public/app'));
-
-
-/* Routes */
 router.get('/login', function *(next){
   yield this.render('login/index',{
     title: 'The Login Page'
   })
 });
+
+router.get('/logout', function *(next){
+  yield this.render('pages/content',{
+    title: 'The Logout Page',
+    body: 'This is the logout page'
+  })
+});
+
+
+/* Distribition Files */
+app.use( mount('/static', serve(__dirname + '/dist')))
+
 
 //shhh.... listen
 app.listen(4040, function () {
