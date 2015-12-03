58241a5ed8dcf6e76c4dd29cb7394bc28ac8fa14
James Kusachi <jamesku@zillow.com>
2015-12-02 23:39:26 -0800

    moving routes to own file

---
+++ config/routes/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var router = require('koa-router')();
+var Post = require('../../app/models/models').Post;
+
+var passport = require('koa-passport')
+router
+
+.get('/login', function *(next){
+
+  var data = yield Post.where('id', 1).fetch();
+
+  var post = data.toJSON();
+
+  yield this.render('login/index',{
+    title: 'The Login Page',
+    body: 'the body'
+  })
+})
+
+.post('/login', passport.authenticate('local'), function *(next){
+  console.log('we got a user');
+})  
+
+.get('/logout', function *(next){
+  yield this.render('pages/content',{
+    title: 'The Logout Page',
+    body: 'This is the logout page'
+  })
+});
+
+module.exports = router;
